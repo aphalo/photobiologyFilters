e7dafdf15f6587b7c162fa376fc0900a7d275a6e
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-04-24 15:39:38 +0300

    Fixed a nasty bug in calc_filter_multipliers related to the pc (%) parameter but behaviour when no argument for pc is given has not changed.

--- R/photobiologyFilters-package.R
+++ R/photobiologyFilters-package.R
@@ -11,8 +11,8 @@
 #' \tabular{ll}{
 #' Package: \tab photobiologyFilters\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.1.4\cr
-#' Date: \tab 2014-04-10\cr
+#' Version: \tab 0.1.5\cr
+#' Date: \tab 2014-04-22\cr
 #' License: \tab GPL (>2.0)\cr
 #' }
 #' @references
