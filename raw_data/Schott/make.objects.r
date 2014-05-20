67e054f0fb783fb7b82ba914128877854fbfc05b
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-05-20 19:30:44 +0300

    Renamed and moved script used to make Schott data objects.

--- raw_data/Schott/make.objects.r
+++ raw_data/Schott/make.objects.r
@@ -1,4 +1,4 @@
 oldwd <- setwd("raw_data/Schott/")
 shell("schott2twocol", "bash")
 setwd("done")
-read.csv("gg400.csv", col.names=c("w.length", "T"), header = FALSE)
+read.csv("gg400.csv", col.names=c("w.length", "Tfr"), header = FALSE)
