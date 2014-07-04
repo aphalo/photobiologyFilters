84e9299ac0c2c97bf2730b055995ae5e2b8fa0f3
Pedro J. Aphalo <pedro.aphalo@helsinki.fi>
2014-07-04 22:33:51 +0300

    First step in adding sheet Plexiglas and other materials from Foiltek.

---
+++ R/foiltek.data.r
@@ -0,0 +1,76 @@
+#' Total transmittance for clear PMMA sheets (Plexiglas) by Evonik
+#'
+#' Total transmittance for Schott optical glass filters.
+#' Data measured with an Agilent~8453 spectrophotometer (based on diode array). Data are
+#' to be used as "reference values only" as values may change in time and production batch.
+#' 
+#' The variables are as follows:
+#' 
+#' \itemize{
+#'   \item w.length (nm)  
+#'   \item Tpc (\%)
+#'   \item Tfr (fraction)
+#' }
+#' 
+#' Sample chips bought from the manufacturers website on 24.04.2014
+#' \url{http://www.plexiglas-shop.com/}
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 911 rows and 3 variables
+#' 
+#' @name Evonik_PC
+#' @aliases UV100.dt PLXUV1.dt PLXUV2.dt PLXUV3.dt PLXUV4.dt PLXUV5.dt
+#' 
+NULL
+
+#' Total transmittance for colour plastic sheets by Evonik
+#'
+#' Total transmittance for Plexiglas sheets 3 mm think.
+#' Data measured with an Agilent~8453 spectrophotometer (based on diode array). Data are
+#' to be used as "reference values only" as values may change in time and production batch.
+#' 
+#' The variables are as follows:
+#' 
+#' \itemize{
+#'   \item w.length (nm)  
+#'   \item Tpc (\%)
+#'   \item Tfr (fraction)
+#' }
+#' 
+#' Sample chips bought from the manufacturers website on 24.04.2014
+#' \url{http://www.plexiglas-shop.com/}
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 911 rows and 3 variables
+#' 
+#' @name Plexiglas_colour
+#' @aliases PLX1C33.dt PLX2C04.dt PLX_3C01.dt PLX5C01.dt
+#' 
+NULL
+#' Total transmittance for clear plastic sheets supplied by Foiltek
+#'
+#' Total transmittance for divers samples of clear plastic sheets).
+#' Data measured with an Agilent~8453 spectrophotometer (based on diode array). Data are
+#' to be used as "reference values only" as values may change in time and production batch.
+#' 
+#' The variables are as follows:
+#' 
+#' \itemize{
+#'   \item w.length (nm)  
+#'   \item Tpc (\%)
+#'   \item Tfr (fraction)
+#' }
+#' 
+#' Sample chips bought from the manufacturers website on 24.04.2014
+#' \url{http://www.plexiglas-shop.com/}
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 911 rows and 3 variables
+#' 
+#' @name Evonik_PC
+#' @aliases PC.dt PC_UV.dt PS.dt Pet_G.dt PVC.dt
+#' 
+NULL
---
+++ raw_data/Foiltek/read.foiltek.csv.files.r
@@ -0,0 +1,27 @@
+# read all available filter data
+# assumes that all files with ".txt" are filter-data files
+# assumes that all files with exactly four columns have two replicate measurements that need
+# to be averaged
+# STEPS
+# 1) clear the workspace
+# 2) read .txt files
+# 3) save all the data.frames created into a single Rda file in data folder
+library(data.table)
+rm(list = ls())
+setwd("raw_data/Foiltek/")
+file.list <- shell('ls *.CSV', intern=TRUE)
+for (file.name in file.list) {
+  dt.name <- paste(sub(pattern=".CSV", replacement="", x=file.name), "dt", sep=".")
+  tmp.df <- read.csv(file.name, skip=1, header=FALSE, col.names=c("w.length", "Tpc", "sd_Tpc"), 
+                     colClasses = c("numeric", "numeric", "NULL"))
+  class(tmp.df) <- c("filter.spct", "generic.spct", class(tmp.df))
+  setDT(tmp.df)
+  setkey(tmp.df, w.length)
+  tmp.dt <- copy(tmp.df)
+  tmp.dt[ , Tfr := Tpc / 100]
+  assign(dt.name, tmp.dt)
+  save(list=dt.name, file=paste("../../data/", dt.name, ".rda", sep=""))
+}
+# rm(list = ls())
+setwd("./../..")
+
